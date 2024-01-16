c4e8486218ef8c2dae3d1533bccb819e0b6d11a3
glackyy <othmanesticky@gmail.com>
2024-01-16 08:51:22 +0100

    minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Calculating the fewest num of operations
+    needed to result in exactly n H
+    chars in the file"""
+    past_chars = 1
+    c = 0
+    pasteboard = 0
+
+    while past_chars < n:
+        if pasteboard == 0:
+            pasteboard = past_chars
+            c += 1
+    
