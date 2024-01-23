926a8eb8b917252e2948894336d35df548a80734
glackyy <othmanesticky@gmail.com>
2024-01-23 22:03:12 +0100

    0-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""Reading stdin line by line and computing metrics"""
+import sys
+
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
