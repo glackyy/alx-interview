fc0a8ea45d365f62b83193a441ed3d3b1f881845
glackyy <othmanesticky@gmail.com>
2024-01-30 10:45:10 +0100

    ValidUTF8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,4 +3,10 @@
 
 
 def validUTF8(data):
-    
+    """Returning True if data is a valid UTF-8
+    encoding, else returning False"""
+    by_count = 0
+    for i in data:
+        if by_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                by_count = 1
