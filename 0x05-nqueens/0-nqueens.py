9fa0630b7c0c01bc1e55a7403cebdcf5a50c91d9
glackyy <othmanesticky@gmail.com>
2024-02-08 09:13:50 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""N Queen Module"""
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
