398b6a93b7de7ddfc0ddde7d6419290994efe28a
glackyy <othmanesticky@gmail.com>
2024-02-15 10:38:24 +0100

    STARWARS

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+const { resolve } = require('path');
 const req = require('request');
 const id_movie = process.argv[2];
 const film_EP = 'https://swapi-api.hbtn.io/api/films/' + id_movie;
@@ -7,13 +8,29 @@ let people = [];
 const names = [];
 
 const requestChar = async () => {
-    await new Promise(resolve => req(film_EP, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-      } else {
-        const jsonBody = JSON.parse(body);
-        people = jsonBody.characters;
-        resolve();
-      }
-    }));
-  };
+  await new Promise(resolve => req(film_EP, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => req(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  }
+}
