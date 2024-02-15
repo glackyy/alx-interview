0967c8600c80959ed502c6f7a2493cde7c896e51
glackyy <othmanesticky@gmail.com>
2024-02-15 10:31:57 +0100

    STARWARS

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+const req = require('request');
+const id_movie = process.argv[2];
+const film_EP = 'https://swapi-api.hbtn.io/api/films/' + id_movie;
+let people = [];
+const names = [];
