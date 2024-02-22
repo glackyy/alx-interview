634a0718228b8f17c3850c28748c024700fb2dfc
glackyy <othmanesticky@gmail.com>
2024-02-22 10:41:00 +0100

    2DMATRIX

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,7 @@
 
 def rotate_2d_matrix(matrix):
     """Rotating a 2d matrix 90° clockwise"""
-    left, right = 0, len(matrix) -1
+    left, right = 0, len(matrix) - 1
     while left < right:
         for i in range(right - left):
             top, bott = left, right
