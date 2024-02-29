54c15c2ce0065d6c5fdbdb80b21b244b358c68ff
glackyy <othmanesticky@gmail.com>
2024-02-29 15:47:09 +0100

    make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,3 +2,15 @@
 """Determining the fewest number of coins needed to meet
 a amount total on a pile of coins if different values"""
 import sys
+
+
+def makeChange(coins, total):
+    """Returning fewest number of coins needed
+    to meet total"""
+    if total <= 0:
+        return 0
+    tab = [sys.maxsize for i in range(total + 1)]
+    tab[0] = 0
+    ma = len(coins)
+    for i in range(1, total + 1):
+        
