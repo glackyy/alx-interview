81fb329c3ee63aa07985ae2946de9ab57205a4c6
glackyy <othmanesticky@gmail.com>
2024-02-29 16:21:37 +0100

    make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,8 +5,11 @@ import sys
 
 
 def makeChange(coins, total):
-    """Returning fewest number of coins needed
-    to meet total"""
+    """
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
     if total <= 0:
         return 0
 
