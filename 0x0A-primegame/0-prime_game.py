c37c90902f94c71ec419aba92fa040cfb2ae7e65
glackyy <othmanesticky@gmail.com>
2024-03-13 11:26:18 +0000

    PRIME GAME

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,4 +7,13 @@ def isWinner(x, nums):
     WCounter = {'Maria': 0, 'Ben': 0}
 
     for i in range(x):
-        
+        rdWinner = isRoundWinner(nums[i], x)
+        if rdWinner is not None:
+            WCounter[rdWinner] += 1
+
+    if WCounter['Maria'] > WCounter['Ben']:
+        return 'Maria'
+    elif WCounter['Ben'] > WCounter['Maria']:
+        return 'Ben'
+    else:
+        return None
