cf323d976df6d0db4bf8c384634a363e3b6e9c65
glackyy <othmanesticky@gmail.com>
2024-03-13 14:35:22 +0000

    PRIME GAME

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,28 +6,25 @@ def isWinner(x, nums):
     """Finding the winner"""
     if not nums or x < 1:
         return None
+    max_n = max(nums)
 
-    max_num = max(nums)
-
-    # Sieve of Eratosthenes
-    sieve = [True] * (max(max_num + 1, 2))
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if sieve[i]:
-            multiples = range(i * i, max_num + 1, i)
-            for multiple in multiples:
-                sieve[multiple] = False
-
-    primes_count = [0]
-    for i in range(1, len(sieve)):
-        if sieve[i]:
-            primes_count.append(primes_count[-1] + 1)
-        else:
-            primes_count.append(primes_count[-1])
-
-    maria_wins = sum(primes_count[num] % 2 == 1 for num in nums)
-
-    return (
-        'Maria' if maria_wins * 2 > x else
-        'Ben' if maria_wins * 2 < x else
-        None
-    )
+    sort = [True for _ in range(max(max_n + 1, 2))]
+    for i in range(2, int(pow(max_n, 0.5)) + 1):
+        if not sort[i]:
+            continue
+        for j in range(i * i, max_n + 1, i):
+            sort[j] = False
+    sort[0] = sort[1] = False
+    y = 0
+    for i in range(len(sort)):
+        if sort[i]:
+            y += 1
+        sort[i] = y
+    pl1 = 0
+    for x in nums:
+        pl1 += sort[x] % 2 == 1
+    if pl1 * 2 == len(nums):
+        return None
+    if pl1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
